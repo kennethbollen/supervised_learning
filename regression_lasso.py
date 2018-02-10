e2366bc43781a13bfa01350c17616a7e80ebd4d7
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-10 22:23:01 +0000

    Create regression_lasso.py

---
+++ regression_lasso.py
@@ -0,0 +1,12 @@
+# Import Lasso
+from sklearn.linear_model import Lasso
+
+# Instantiate a lasso regressor: lasso
+lasso = Lasso(alpha=0.4, normalize=True)
+
+# Fit the regressor to the data
+lasso.fit(X, y)
+
+# Compute and print the coefficients
+lasso_coef = lasso.coef_
+print(lasso_coef)
