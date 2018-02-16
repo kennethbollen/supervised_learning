fe2b103d116ca628e49e41f810950a1c2da06ce4
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-16 17:58:35 +0000

    Update regression_linear.py

--- regression_linear.py
+++ regression_linear.py
@@ -1,48 +1,37 @@
-import numpy as np
-import pandas as pd
-# Import LinearRegression
+from data_extract import *
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import numpy as np
+import matplotlib.pyplot as plt
 
+steps = [('scaled', StandardScaler()), ('linear_regression', LinearRegression())]
 
-# Create arrays for features and target variable
-y = np.array(df['life'])
-X = np.array(df['fertility'])
+pipeline = Pipeline(steps)
 
-# Reshape X and y
-y = y.reshape(-1,1)
-X = X.reshape(-1,1)
+#Create train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Create training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
+#Compute 5 fold cross validation score
+cv_scores = cross_val_scores(pipeline, X, y, cv=5)
+print('5 fold cross validation score: {}'.format(cv_scores))
+print('Average cross validation score: {}'.format(np.mean(cv_scores)))
 
-# Create the regressor: reg
-reg = LinearRegression()
+#fit the model to the data
+pipeline.fit(X_train, y_train)
 
-# Compute 5-fold cross-validation scores: cv_scores
-cv_scores = cross_val_score(reg, X, y, cv=5)
+#compute predictions over the prediction space
+y_pred = pipeline.predict(prediction_space)
 
-# Print the 5-fold cross-validation scores
-print(cv_scores)
+#R^2
+print('R^2: {}'.format(pipeline.score(X_test, y_test)))
+#RMSE
+rmse np.sqrt(mean_squared_error(y_test, y_pred)
+print('Root mean squared error: {}'.format(rmse))
 
-print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
-
-# Create the prediction space
-prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1,1)
-
-# Fit the model to the data
-reg.fit(X_fertility, y)
-
-# Compute predictions over the prediction space: y_pred
-y_pred = reg.predict(prediction_space)
-
-# Print R^2 and RMSE
-print(reg.score(X_fertility, y))
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-print("Root Mean Squared Error: {}".format(rmse))
-
-# Plot regression line
+#plot regression line
 plt.plot(prediction_space, y_pred, color='black', linewidth=3)
 plt.show()
